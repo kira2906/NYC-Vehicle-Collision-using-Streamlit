3b151ca291c72fdf70b03a962c8e05ef03a12e4d
Raihan Rasheed <raihan.hardy@gmail.com>
2023-10-27 15:29:16 +0300

    Update NYCMotorCollisionwithStreamlit.py

--- NYCMotorCollisionwithStreamlit.py
+++ NYCMotorCollisionwithStreamlit.py
@@ -35,9 +35,59 @@ else:
     st.error("Failed to download the data. Please check the URL or try again later.")
     
 # Title Section
-st.title("Motor Vehicle Collisions in New York City")
-st.markdown("This application is a Streamlit dashboard that can be used to analyze motor vehicle collisions in NYC")
-st.markdown("Data Source: https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95")
+# Center-align the title using HTML/CSS
+st.markdown(
+    """
+    <h1 style='text-align: center;'>NYC🗽 Vehicle Collision Data Explorer 🚗💥🚗</h1>
+    """,
+    unsafe_allow_html=True
+)
+
+# Insert the GIF
+gif_url = "https://media.tenor.com/Cy7rKC7i-6gAAAAC/joseph-gordon-levitt-premium-crush.gif"
+st.markdown(f'<p style="text-align:center"><img src="{gif_url}"></p>', unsafe_allow_html=True)
+st.markdown(f'<p style="text-align:center">*Developed by Raihan Rasheed*</p>', unsafe_allow_html=True)
+st.markdown("""
+
+
+This Streamlit application allows you to explore and analyze motor vehicle collision data in New York City (NYC). It provides insights into accident locations, injury and fatality statistics, contributing factors, and common vehicle types involved in collisions.
+
+## Data Source
+
+The data used in this application is sourced from the [NYC Open Data platform](https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95).
+
+## Sections
+
+### Section 1 - Geo Spatial Analysis of Injured People
+
+1. **Map of Injured People**: This section allows you to filter and visualize locations where a specific number of people were injured in vehicle collisions on a map.
+
+2. **Map of Fatalities**: Similar to the previous section, this one visualizes locations where a certain number of people were killed in collisions.
+
+3. **Collisions by Time of Day**: This part shows the number of collisions that occurred during a given hour of the day. It includes a histogram and a map.
+
+### Section 2 - In depth Analysis of Accidents in NYC
+
+1. **Top Dangerous Streets**: You can choose the affected type of people (Pedestrians, Cyclists, or Motorists) and see the top 5 dangerous streets for that type.
+
+2. **Top Dangerous Boroughs**: Similar to the previous section, but it focuses on the most dangerous boroughs by the affected type of people.
+
+3. **Most Common Contributing Factors**: It displays the most common contributing factors in vehicle accidents in NYC.
+
+4. **Most Common Vehicle Types**: This part reveals the most common vehicle types involved in accidents.
+
+5. **Vehicle Accidents by Type**: You can select a specific vehicle type, and the map will display the locations of accidents involving that type.
+
+## How to Use
+
+Simply run this Streamlit application and explore the various sections by selecting the options available. You can interact with maps, charts, and data to gain insights into motor vehicle collisions in NYC.
+
+
+
+*Note: The application provides visualizations and insights based on available data. Use it for informational purposes and to understand trends in vehicle collisions.*
+
+            """)
+
 
 # Data Cleansing
 @st.cache_data(persist=True)
@@ -55,16 +105,21 @@ def load_data(nrows):
 data = load_data(100000)
 original_data = data
 
+st.write("""
+         ---
+         ## Section 1 - Geo Spatial Analysis of Injured People
+         """)
+
 # Section 1 - Geo Spatial Analysis of Injured People
-st.header("Where are the most people injured in NYC?")
+st.write("### Where are the most people injured in NYC?")
 injured_people = st.slider("Number of persons injured in vehicle collisions", 1, 19)
 st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
 
-st.header("Where are the most people killed in NYC?")
+st.write("### Where are the most people killed in NYC?")
 killed_people = st.slider("Number of persons killed in vehicle collisions", 1, 4)
 st.map(data.query("killed_persons >= @killed_people")[["latitude", "longitude"]].dropna(how="any"))
 
-st.header("How many collisions occur during a given time of day?")
+st.write("### How many collisions occur during a given time of day?")
 hour = st.slider("Hour to look at", 0, 23)
 data = data[data['date/time'].dt.hour == hour]
 st.markdown(f'Vehicle collisions between {hour}:00 and {hour + 1}:00')
@@ -99,8 +154,14 @@ chart_data = pd.DataFrame({'minute': range(60), 'crashes': hist})
 fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height=400)
 st.write(fig)
 
+
+st.write("""
+         ---
+         ## Section 2 - In depth Analysis of Accidents in NYC
+         """)
+
 # Section 2 - Graphical Analysis of Accidents in NYC
-st.header('Top 5 Dangerous streets by affected type')
+st.write("### Top 5 Dangerous streets by affected type")
 select = st.selectbox('Affected Type of People', 
                       ['Pedestrians', 'Cyclists', 'Motorists'])
 if select == 'Pedestrians':
@@ -120,7 +181,7 @@ top_5_dangerous_streets = dangerous_streets_count.head(5)
 table_html = top_5_dangerous_streets.to_html(index=False, justify='center')
 st.markdown(table_html, unsafe_allow_html=True)
 
-st.header('Top 5 Dangerous Boroughs by affected type')
+st.write("### Top 5 Dangerous Boroughs by affected type")
 select_typeforboroughs = st.selectbox('Affected Type of People', 
                                       ['Pedestrians', 'Cyclists', 'Motorists'], 
                                       key='unique_key_here')
@@ -138,7 +199,7 @@ borough_data_count = borough_data_count.sort_values(by='Number of Incidents', as
 # Display the top 5 dangerous boroughs without the index column
 st.markdown(borough_data_count.head(5).to_html(index=False, justify='center'), unsafe_allow_html=True)
 
-st.header("Most Common Contributing Factors in Accidents")
+st.write("### Most Common Contributing Factors in Accidents")
 # Extract contributing factor columns from the data in lowercase
 contributing_factors = ['contributing_factor_vehicle_1', 
                         'contributing_factor_vehicle_2', 
@@ -153,7 +214,7 @@ factor_counts = factor_counts.value_counts().reset_index(name='count').rename(co
 fig = px.bar(factor_counts.head(10), x='Contributing Factor', y='count', labels={'Contributing Factor': 'Factor', 'count': 'Frequency'})
 st.plotly_chart(fig)
 
-st.header("Most Common Vehicle Types in Accidents")
+st.write("### Most Common Vehicle Types in Accidents")
 # Define the columns for vehicle types
 vehicle_type_columns = ['vehicle_type_1', 
                         'vehicle_type_2', 
@@ -178,7 +239,7 @@ fig = px.pie(vehicle_type_counts.head(10), names='Vehicle Type', values='count',
              labels={'Vehicle Type': 'Type', 'count': 'Frequency'})
 st.plotly_chart(fig)
 
-st.header("Where are the most common vehicle accidents by vehicle type in NYC?")
+st.write("### Where are the most common vehicle accidents by vehicle type in NYC?")
 for column in vehicle_type_columns:
     data[column] = data[column].replace(renaming_mapping)
 # Create a list of unique vehicle types
@@ -201,3 +262,14 @@ st.map(filtered_data[['latitude', 'longitude']].dropna(how="any"))
 if st.checkbox("Show Raw Data", False):
     st.subheader('Raw Data')
     st.write(data)
+    
+st.markdown('''
+            ---
+#### Find the source code on GitHub
+[GitHub Repository](https://github.com/kira2906/NYC-Vehicle-Collision-using-Streamlit/tree/main)
+
+#### Ready to collaborate on data science and ML projects
+*If you have similar projects or need assistance with data science and machine learning tasks, feel free to [contact me](mailto:mlsolutions.raihan@gmail.com) I'm available for freelance work and collaboration!*
+''')   
+    
+    
